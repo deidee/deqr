80e6a63cdecbc2ec2a701e8f99ba9cf71d1fba22
ACJ <gitlab@acjs.net>
2021-11-19 22:48:13 +0100

    deJade PoC

--- tests/hallo-wereld.php
+++ tests/hallo-wereld.php
@@ -1,17 +1,18 @@
 <?php
 
 require '../vendor/autoload.php';
+require '../src/Writer/SvgWriter.php';
 
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Color\Color;
-use Endroid\QrCode\Writer\SvgWriter;
+use deidee\deQR\Writer\SvgWriter;
 
 $writer = new SvgWriter;
 
-$qrCode = QrCode::create('http://jeidee.nl/')
+$qrCode = QrCode::create('https://jeidee.nl/')
     ->setSize(300)
     ->setMargin(10)
-    ->setForegroundColor(new Color(0, 0, 0))
+    ->setForegroundColor(new Color(0, 0, 0, .5))
     ->setBackgroundColor(new Color(255, 255, 255));
 
 $result = $writer->write($qrCode);
